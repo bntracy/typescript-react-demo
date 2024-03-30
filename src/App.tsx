ed5689316994f6a7f1b7b375d18ae0019b1f3185
Beth Tracy <bntracy@gmail.com>
2024-03-30 16:43:08 -0500

    spirit randomizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { AppProps, Spirit } from './App.types';
 import SpiritList from './components/SpiritList';
 
 const App: FC<AppProps> = ({ title }) => {
+  const [randomSpirit, setRandomSpirit] = useState<Spirit | null>(null);
+
   const spirits: Spirit[] = [];
   spirits.push({name: 'Lightning\'s Swift Strike', aspects: ['(no aspect)', 'Immense', 'Pandemonium', 'Sparking', 'Wind']});
   spirits.push({name: 'River Surges in Sunlight', aspects: ['(no aspect)', 'Haven', 'Sunshine', 'Travel']});
@@ -10,10 +12,18 @@ const App: FC<AppProps> = ({ title }) => {
   spirits.push({name: 'Vital Strength of the Earth', aspects: ['(no aspect)', 'Might', 'Nourishing', 'Resilience']});
   console.log(spirits);
 
+  const randomizeSpirit = () => {
+    const length = spirits.length;
+    const randomIndex = Math.floor(Math.random() * length); // returns a random number between 0 and length-1 inclusive
+    setRandomSpirit(spirits[randomIndex]);
+  }
+
   return (
     <div>
         <h1>{title}</h1>
         <SpiritList spirits={spirits} />
+        <button type="button" onClick={randomizeSpirit}>Random Spirit</button>
+        {randomSpirit && <div>{randomSpirit.name}</div>}
     </div>
   );
 };
