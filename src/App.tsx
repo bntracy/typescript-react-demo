8a3de6df435560c851782ff5766506007c8b53e8
Beth Tracy <bntracy@gmail.com>
2024-03-30 14:07:36 -0500

    random users display

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
-import { FC } from 'react';
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
 
 interface AppProps {
   title: string;
 }
 
-const App: FC<AppProps> = ({ title }) => {
-  return <h1>{title}</h1>;
+export interface Name {
+  first: string;
+  last: string;
 }
+
+export interface Login {
+  uuid: string;
+}
+
+export interface Users {
+  name: Name;
+  login: Login;
+  email: string;
+}
+
+const App: FC<AppProps> = ({ title }) => {
+  const [users, setUsers] = useState<Users[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const { data } = await axios.get(
+          'https://randomuser.me/api/?results=10'
+        );
+        console.log(data);
+        setUsers(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUsers();
+  }, []);
+  
+  return (
+    <div>
+        <h1>{title}</h1>
+        <ul>
+          {users.map(({ login, name, email }) => {
+            return (
+              <li key={login.uuid}>
+                <div>
+                  Name: {name.first} {name.last}
+                </div>
+                <div>Email: {email}</div>
+                <hr />
+              </li>
+            );
+          })}
+        </ul>
+    </div>
+  );
+};
 
 export default App;
